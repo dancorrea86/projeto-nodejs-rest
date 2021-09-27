25885dfeb576fa33852a58668d9237a9640bd04d
dancorrea86 <danielmacielcorrea@gmail.com>
2021-09-27 17:01:49 -0300

    Model para conexão e configuração do envio de dados para o Banco de Dados MySQL

---
+++ models/atendimentos.js
@@ -0,0 +1,27 @@
+const moment = require('moment')
+
+const conexao = require('../infraestrutura/conexao')
+
+class Atendimento {
+    adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+
+        console.log(atendimento)
+
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
+        console.log(atendimentoDatado)
+        
+        const sql = 'INSERT INTO Atendimentos SET ?'
+
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+            if(erro) {
+                console.log(erro)
+            } else {
+                console.log(resultados)
+            }
+        })
+    }
+}
+
+module.exports = new Atendimento
